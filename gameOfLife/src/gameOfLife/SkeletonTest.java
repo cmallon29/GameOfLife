60fd978084dfeb9f3319379c172d58fb90465b3f
40132811 <40132811@8aab5721-ba9a-9a40-829a-44a3a879d100>
2016-05-04 17:33:17 +0000

    40132811- testing

--- gameOfLife/src/gameOfLife/SkeletonTest.java
+++ gameOfLife/src/gameOfLife/SkeletonTest.java
@@ -2,6 +2,8 @@ package gameOfLife;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class SkeletonTest{
@@ -72,35 +74,59 @@ public class SkeletonTest{
 //		fail("Not yet implemented");
 //	}
 
+//	@Test
+//	public void testShowUniCards(int response) {
+//		Skeleton tester = new Skeleton();
+//		if(tester.showUniCards(int response))
+//		{
+//			tester.showUniCards(0);
+//			tester.showUniCards(1);
+//			tester.showUniCards(2);
+//			tester.showUniCards(3);
+//			tester.showUniCards(4);
+//			tester.showUniCards(5);
+//		}
+//		else 
+//		{
+//			fail("Not yet implemented");
+//		}
+//		
+//	}
+	
+	
+
 	@Test
-	public void testShowUniCards(int response) {
-		Skeleton tester = new Skeleton();
-		if(tester.showUniCards()){
-			tester.showUniCards(0);
-			tester.showUniCards(1);
-			tester.showUniCards(2);
-			tester.showUniCards(3);
-			tester.showUniCards(4);
-			tester.showUniCards(5);
-		}
-		else {
-			fail("Not yet implemented");
-		}
-		
+	public void testAddAndGet1() {
+	    ArrayList list = new ArrayList();
+	    list.add(42);
+	    list.add(-3);
+	    list.add(17);
+	    list.add(99);
+	    assertEquals(42, list.get(0));
+	    assertEquals(-3, list.get(1));
+	    assertEquals(17, list.get(2));
+	    assertEquals(99, list.get(3));
+
+	    assertEquals("second attempt", 42, list.get(0));   // make sure I can get them a second time
+	    assertEquals("second attempt", 99, list.get(3));
 	}
 
 //	@Test
 //	public void testShowCareerCards() {
-//		Skeleton tester = new Skeleton();
-//		fail("Not yet implemented");
-//	}
+//		
+////		test cards 
+//		Skeleton cards = new Skeleton();
 //
+//	
+//		
+//		
+//	}
 //	@Test
-//	public void testUpdatePos() {
+//	public void testUpdatePos(int movement) {
 //		Skeleton tester = new Skeleton();
 //		fail("Not yet implemented");
 //	}
-//
+
 //	@Test
 //	public void testUpdateCar() {
 //		Skeleton tester = new Skeleton();
